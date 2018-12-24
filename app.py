2f9dd7f3942508c2a6ca61410d33859e51032f06
keemsisi <keemsisi@gmail.com>
2018-12-24 12:56:20 +0100

    Update app.py
    This new change is the optimal solution for the fee formulation software. Comparing it with the values on the chapter 25 we can see that the solution is very similar and offers the best way to formulate the feed for the animal.

--- app.py
+++ app.py
@@ -10,35 +10,47 @@ def home():
 @app.route('/test')
 def compute():
 
-    # All the (symbolic) variables are declared, with a name and optionally a lower and/or upper bound.
+   x1 = Variable('x1', lb=0)
+x2 = Variable('x2', lb=0)
+x3 = Variable('x3', lb=0)
+x4 = Variable('x4', lb=0)
+x5 = Variable('x5', lb=0)
+x6 = Variable('x6', lb=0)
+x7 = Variable('x7', lb=0)
+x8 = Variable('x8', lb=0)
+x9 = Variable('x9', lb=0)
 
-    x1 = Variable('x1', lb=0)
-    x2 = Variable('x2', lb=0)
-    x3 = Variable('x3', lb=0)
-    x4 = Variable('x4', lb=0)
-    x5 = Variable('x5', lb=0)
-    x6 = Variable('x6', lb=0)
-    x7 = Variable('x7', lb=0)
+# LOWER BOUND >=
+#UPPER BOUND <=
 
-    # A constraint is constructed from an expression of variables and a lower and/or upper bound (lb and ub).
+# A constraint is constructed from an expression of variables and a lower and/or upper bound (lb and ub).
+# The contraints wiht = sign should be reconstructed to have two new contraints with >= and <=
 
-    c1 = Constraint(0.08*x1 + 0.01*x2 + 0.1*x3 + 0.11*x4 + 0.1*x5, ub=0.11)
-    c2 = Constraint(x1 + x2, ub=55)
-    c3 = Constraint(x3 + x4, ub=40)
-    c4 = Constraint(x5, ub=22)
-    c5 = Constraint(x6, ub=2)
-    c6 = Constraint(x7, ub=1)
-    c7 = Constraint(x1 + x2 + x3 + x4 + x5 + x6 + x7, ub=100)
+c1 = Constraint(x1+ x2+ x3+ x4+ x5+ x6+ x7+ x8+ x9,lb= 100 )
+c2 = Constraint(0.088*x1+0.44*x2+0.157*x3+ 0*x4 + 0*x5 + 0*x6 + 0.6*x7+0.6*x8 + 0*x9, lb=20.87)
+c3 = Constraint(0.04*x1 +0.035*x2 + 0*x3 + 0*x4 + 0*x5 + 0*x6 + 0*x7 + 0*x8 + 0*x9 ,ub= 3.55)
+c4 = Constraint(0.02*x1+0.065*x2+0.051*x3 + 0*x4 + 0*x5 + 0*x6 + 0*x7 + 0*x8 + 0*x9 ,ub= 4.09)
+c5 = Constraint(0.0001*x1+0.002*x2+0.0014*x3+0.21*x4+0.38*x5 + 0*x6 + 0*x7 + 0*x8 + 0*x9 ,ub= 1.86)
+c6 = Constraint(0.0009*x1+0.002*x2+0.0115*x3+0.185*x4+0.015*x5 + + 0*x6 + 0*x7 + 0*x8 + 0*x9 ,lb=0.58 )
+c7 = Constraint(0.0025*x1+0.028*x2+0.0059*x3 + 0*x5 + 0*x6 + x7 + 0*x8 + 0*x9,lb= 1.26 )
+c8 = Constraint(0.0018*x1+0.0059*x2+0.0042*x3+0*x5 + 0*x6 + 0*x7 + x8 + 0*x9,lb=0.42 )
+c9 = Constraint(3432*x1 + 2230*x2 + 1300*x3+ + 0*x4 + 0*x5 + 0*x6 + 0*x7 + 0*x8 + 0*x9,lb= 2826.39)
+c10 = Constraint(0*x1 + 0*x2 + 0*x3 + 0*x4 + 0*x5 + x6 + 0*x7 + 0*x8 + 0*x9, ub=0.3)
+c11 = Constraint(0*x1 + 0*x2 + 0*x3 + 0*x4 + 0*x5 + 0*x6 + 0*x7 + 0*x8 + x9, ub=0.3)
+c12 = Constraint(0.0001*x1+0.002*x2+0.0014*x3+0.21*x4+0.38*x5 + 0*x6 + 0*x7 + 0*x8 + 0*x9 ,lb= 1.86)
 
-    # An objective can be formulated
-    obj = Objective(8.5 * x1 + 6 * x2 + 15 * x3 + 13 * x4 + 10 * x5 + 2 *40 + 1 *5 ,direction='min')
 
-    # Variables, constraints and objectives are combined in a Model object, which can subsequently be optimized.
-    model = Model(name='Simple model')
-    model.objective = obj
-    model.add([c1, c2, c3, c4, c5, c6, c7])
-    status = model.optimize()
 
+
+# An objective can be formulated
+obj = Objective( 58*x1+150*x2+60*x3+15*x4+50*x5+90*x6+700*x7+1300*x8+550*x9
+ ,direction='min')
+
+# Variables, constraints and objectives are combined in a Model object, which can subsequently be optimized.
+model = Model(name='Simple model')
+model.objective = obj
+model.add([c1, c2, c3,c4,c5,c6,c7,c8,c9,c10,c11,c12])
+status = model.optimize()
     print("status:", model.status)
     print("objective value:", model.objective.value)
     print("-------------")
